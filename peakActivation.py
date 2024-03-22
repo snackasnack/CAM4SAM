02d09f9b63629aa1b5cf16a26410d2cdb6adfc64
snackasnack <snackaddict@gmail.com>
2024-03-22 21:10:32 +0800

    updated file

--- peakActivation.py
+++ peakActivation.py
@@ -4,13 +4,13 @@ from skimage.feature import graycomatrix, graycoprops
 import cv2
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import load_model
 
 
-def read_results(directory_path = 'C:/Users/snack/Desktop/CAM4SAM/temp_files/Saved_Results/mask_results'):
+def read_results(directory_path = 'C:/Users/snack/Desktop/CAM4SAM/temp_files/Saved_Results/mask_results_experiment_combi'):
     data = []
     files = [f for f in os.listdir(directory_path) if f.endswith('.txt')]
     for file_name in files:
@@ -24,10 +24,11 @@ def read_results(directory_path = 'C:/Users/snack/Desktop/CAM4SAM/temp_files/Sav
     return pd.DataFrame(data, columns=['file_name','IoU_Result', 'CAM_Threshold', 'Sobel_Threshold'])
 
 
-def texture_analysis(directory_path = 'C:/Users/snack/Desktop/CAM4SAM/temp_files/Saved_Results/mask_results'):
+def texture_analysis(directory_path = 'C:/Users/snack/Desktop/CAM4SAM/temp_files/Saved_Results/mask_results_experiment_combi', files = None):
     texture_data = []
 
-    files = [f for f in os.listdir(directory_path) if f.endswith('.txt')]
+    if files is None:
+        files = [f for f in os.listdir(directory_path) if f.endswith('.txt')]
     image_folder = 'C:/Users/snack/Desktop/SEAM/VOC2012_data/JPEGImages/'
     #image_folder = '/Users/teyc/Desktop/VOCdevkit 2/VOC2012/JPEGImages/'
     for file_name in files:
@@ -45,12 +46,14 @@ def texture_analysis(directory_path = 'C:/Users/snack/Desktop/CAM4SAM/temp_files
         texture_data.append((file_name[:11], float(contrast), float(correlation), float(energy), float(homogeneity)))
 
     texture_df = pd.DataFrame(texture_data, columns=['file_name', 'contrast', 'correlation', 'energy', 'homogeneity'])
-    scaler = MinMaxScaler()
-    X_scaled = scaler.fit_transform(texture_df[['contrast', 'correlation', 'energy', 'homogeneity']])
-    scaled_df = pd.DataFrame(X_scaled, columns=['contrast', 'correlation', 'energy', 'homogeneity'])
-    texture_df[['contrast', 'correlation', 'energy', 'homogeneity']] = scaled_df
+    file_name_column = texture_df[['file_name']]
+    numerical_columns = texture_df[['contrast', 'correlation', 'energy', 'homogeneity']]
+    scaler = StandardScaler()
+    scaled_numerical_columns = scaler.fit_transform(numerical_columns)
+    scaled_numerical_df = pd.DataFrame(scaled_numerical_columns, columns=numerical_columns.columns)
+    scaled_df = pd.concat([file_name_column, scaled_numerical_df], axis=1)
 
-    return texture_df, scaler
+    return scaled_df, scaler
 
 
 def mergeDf(experiment_data, texture_data):
@@ -70,30 +73,29 @@ def multi_layer_perceptron(optimised_data):
     X = optimised_data[['contrast', 'correlation', 'energy', 'homogeneity']]
     y = optimised_data[['CAM_Threshold', 'Sobel_Threshold']]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=8)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=8)
     input_size = X_train.shape[1]
     model = Sequential()
     model.add(Dense(16, activation='relu', input_shape=(input_size,)))
-    model.add(Dense(32, activation='relu'))
-    model.add(Dense(16, activation='relu'))
+    model.add(Dense(8, activation='relu'))
     model.add(Dense(2, activation='linear'))
-    model.compile(loss='mean_squared_error', optimizer='sgd')
+    model.compile(loss='mean_squared_error', optimizer='adam')
     model.fit(X_train, y_train, epochs=50, batch_size=1, verbose=1)
 
     score = model.evaluate(X_test, y_test, verbose=1)
     print('\n', 'Test MSE:', score)
 
-    model.save("texture_model.keras")
+    model.save("texture_model.h5")
 
 
 def predictThresholds(X_inputs):
-    saved_model = load_model("texture_model.keras")
+    saved_model = load_model("texture_model.h5")
     
     return saved_model.predict(X_inputs)
 
-
+'''
 if __name__ == "__main__":
-
+    
     experiment_df = read_results()
     #print(experiment_df)
     #results_df['combi'] = results_df['CAM_Threshold'].astype(str) + "_" + results_df['Sobel_Threshold'].astype(str)
@@ -102,21 +104,13 @@ if __name__ == "__main__":
     #print(texture_df)
 
     optimised_data = getOptimisedData(experiment_df, texture_df)
-    print(optimised_data)
+    #print(optimised_data)
     
     multi_layer_perceptron(optimised_data)
-
+    
     #selected_data = texture_df[['contrast', 'correlation', 'energy', 'homogeneity']].iloc[[40]]
     selected_data = texture_df[['contrast', 'correlation', 'energy', 'homogeneity']].iloc[:]
-    selected_data_normalized = scaler.transform(selected_data)
+    #selected_data_normalized = scaler.transform(selected_data)
     #print(selected_data_normalized)
-    print(predictThresholds(selected_data_normalized))
-    
-    '''
-    all_combi = results_df['combi'].unique()
-    filtered_df = results_df[results_df['IoU_Result'] == 0]
-    combi_with_zero = filtered_df['combi'].unique()
-    difference = np.setdiff1d(combi_with_zero, all_combi)
-    print("Unique 'combi' values in filtered_df but not in results_df:")
-    print(difference)
-    '''
+    print(predictThresholds(selected_data))
+'''
