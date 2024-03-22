71b3a6739a07fa5ae0bbdcb6fd406365b2395df7
snackasnack <snackaddict@gmail.com>
2024-03-22 19:06:29 +0800

    added a MLP to train a NN to predict thresholds for peak activation

--- peakActivation.py
+++ peakActivation.py
@@ -1,10 +1,13 @@
 import os 
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
-import numpy as np
 from skimage.feature import graycomatrix, graycoprops
 import cv2
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import load_model
 
 
 def read_results(directory_path = 'C:/Users/snack/Desktop/CAM4SAM/temp_files/Saved_Results/mask_results'):
@@ -26,6 +29,7 @@ def texture_analysis(directory_path = 'C:/Users/snack/Desktop/CAM4SAM/temp_files
 
     files = [f for f in os.listdir(directory_path) if f.endswith('.txt')]
     image_folder = 'C:/Users/snack/Desktop/SEAM/VOC2012_data/JPEGImages/'
+    #image_folder = '/Users/teyc/Desktop/VOCdevkit 2/VOC2012/JPEGImages/'
     for file_name in files:
         image_path = os.path.join(image_folder + file_name[:11]+ '.jpg')
         loaded_image = cv2.imread(image_path)
@@ -40,17 +44,74 @@ def texture_analysis(directory_path = 'C:/Users/snack/Desktop/CAM4SAM/temp_files
         homogeneity = graycoprops(glcm, 'homogeneity')
         texture_data.append((file_name[:11], float(contrast), float(correlation), float(energy), float(homogeneity)))
 
-    return pd.DataFrame(texture_data, columns=['file_name','contrast', 'correlation', 'energy', 'homogeneity'])
+    texture_df = pd.DataFrame(texture_data, columns=['file_name', 'contrast', 'correlation', 'energy', 'homogeneity'])
+    scaler = MinMaxScaler()
+    X_scaled = scaler.fit_transform(texture_df[['contrast', 'correlation', 'energy', 'homogeneity']])
+    scaled_df = pd.DataFrame(X_scaled, columns=['contrast', 'correlation', 'energy', 'homogeneity'])
+    texture_df[['contrast', 'correlation', 'energy', 'homogeneity']] = scaled_df
+
+    return texture_df, scaler
+
+
+def mergeDf(experiment_data, texture_data):
+    return pd.merge(experiment_data, texture_data, on='file_name')
+
+
+def getOptimisedData(experiment_df, texture_df):
+    mergedData = mergeDf(experiment_df, texture_df)
+    top_2_iou_results = mergedData.groupby('file_name')['IoU_Result'].nlargest(2)
+    top_2_iou_results = top_2_iou_results.reset_index()
+    top_2_rows = mergedData.loc[top_2_iou_results.level_1]
     
+    return top_2_rows
+
+
+def multi_layer_perceptron(optimised_data):
+    X = optimised_data[['contrast', 'correlation', 'energy', 'homogeneity']]
+    y = optimised_data[['CAM_Threshold', 'Sobel_Threshold']]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=8)
+    input_size = X_train.shape[1]
+    model = Sequential()
+    model.add(Dense(16, activation='relu', input_shape=(input_size,)))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(2, activation='linear'))
+    model.compile(loss='mean_squared_error', optimizer='sgd')
+    model.fit(X_train, y_train, epochs=50, batch_size=1, verbose=1)
+
+    score = model.evaluate(X_test, y_test, verbose=1)
+    print('\n', 'Test MSE:', score)
+
+    model.save("texture_model.keras")
+
+
+def predictThresholds(X_inputs):
+    saved_model = load_model("texture_model.keras")
+    
+    return saved_model.predict(X_inputs)
+
+
 if __name__ == "__main__":
 
-    results_df = read_results()
-    print(results_df)
+    experiment_df = read_results()
+    #print(experiment_df)
     #results_df['combi'] = results_df['CAM_Threshold'].astype(str) + "_" + results_df['Sobel_Threshold'].astype(str)
     
-    texture_df = texture_analysis()
-    print(texture_df)
+    texture_df, scaler= texture_analysis()
+    #print(texture_df)
 
+    optimised_data = getOptimisedData(experiment_df, texture_df)
+    print(optimised_data)
+    
+    multi_layer_perceptron(optimised_data)
+
+    #selected_data = texture_df[['contrast', 'correlation', 'energy', 'homogeneity']].iloc[[40]]
+    selected_data = texture_df[['contrast', 'correlation', 'energy', 'homogeneity']].iloc[:]
+    selected_data_normalized = scaler.transform(selected_data)
+    #print(selected_data_normalized)
+    print(predictThresholds(selected_data_normalized))
+    
     '''
     all_combi = results_df['combi'].unique()
     filtered_df = results_df[results_df['IoU_Result'] == 0]
