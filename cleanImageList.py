300b94aca3b16968154409c77a55baa120083034
snackasnack <snackaddict@gmail.com>
2024-03-23 10:49:35 +0800

    updated cleanImageList.py

--- cleanImageList.py
+++ cleanImageList.py
@@ -1,27 +1,28 @@
 import os 
 
-file_path = "C:/Users/snack/Desktop/CAM4SAM/temp_files/classZeroSingleInstanceImages.txt"
+file_path = "C:/Users/snack/Desktop/CAM4SAM/temp_files/allClassesSegmented.txt"
 updated_lines = []
 
 with open(file_path, "r") as file:
     for line in file:
         line = line.strip()
         file_name = line.replace(".npy", "")
-        print(file_name)
+        #print(file_name)
         
         try:
-            # Attempt to read the file path
+            # If image does not have its segmented form
             image_path = os.path.join('C:/Users/snack/Desktop/SEAM/VOC2012_data/SegmentationClass/', file_name + '.png')
             with open(image_path, "rb"):
-                pass  # If successful, do nothing
+                pass 
+            # If image does not have its .npy form
+            image_path = os.path.join('C:/Users/snack/Desktop/SEAM/voc12/out_cam/', file_name + '.npy')
+            with open(image_path, "rb"):
+                pass 
         except FileNotFoundError:
-            # If the file is not found, remove the corresponding file name from the list
             print(f"Removing {file_name} from the list")
         else:
-            # If no exception occurred, keep the line in the updated list
-            updated_lines.append(line)
+            updated_lines.append(file_name)
 
-# Write back the updated lines to the file
 with open(file_path, "w") as file:
     for line in updated_lines:
         file.write(line + "\n")
